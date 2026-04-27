af2ce18ff3dce06bbbb7d9ab53122eaf74c35bb2
Glenn Hurley <glenn@Glenns-MacBook-Air.local>
2026-04-26 22:14:04 -0400

    fresh start: ghostty, starship, zsh, brewfile

---
+++ Brewfile
@@ -0,0 +1,3 @@
+brew "starship"
+cask "font-jetbrains-mono-nerd-font"
+cask "ghostty"
--- bash/lnbash.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-ln -rs .bash_profile ~/.bash_profile
-ln -rs .bash_prompt ~/.bash_prompt
-ln -rs .bashrc ~/.bashrc
